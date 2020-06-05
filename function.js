3aac9360d738631e706cffae148df576b57a9620
Albert Peralta <a1peralt@ucsd.edu>
2020-06-04 17:23:20 -0700

    Soda edits and Nurture images

---
+++ function.js
@@ -0,0 +1,38 @@
+/*JS File*/
+
+
+$(document).ready(function() {
+
+	/* Progress Bar based on the container with all the content*/
+	$('#scroll').scroll(function () {
+  		var s = $(this).scrollTop(),
+        d = $(this).get(0).scrollHeight,
+        c = $(this).height();
+        var scrollPercent = (s / (d-c)) * 100;
+
+   		$("#progress").css({"height":scrollPercent+'%'});
+
+	});
+
+
+	/*Dynamic ScrollTo*/
+	$('.chapter').click( function(){
+		let id = $(this).attr('id');
+		id = '#'+id+'-text';
+		$("#scroll").animate({ scrollTop: locations[id] + 'px'}, 'swing');
+	});
+
+});
+
+
+var locations = {
+	'#OV-text': 570,
+	'#PS-text': 823,
+	'#NF-text': 951,
+	'#UP-text': 1204,
+	'#CA-text': 4136,
+	'#LFP-text': 4381,
+	'#MFP-text': 5737,
+	'#HFP-text': 9097,
+	'#FN-text': 10355
+};
